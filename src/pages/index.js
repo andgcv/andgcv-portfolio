6eb6c01d506df1d64027e8fb0cbe06835c446c40
André Gonçalves <andgcv@gmail.com>
2019-11-16 18:14:03 +0000

    In the process of building the skeleton of the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+        <h1>Hi there!</h1>
+        <h1>I'm André</h1>
+        <h2>An ambitious, hands-on Back-end Engineer!</h2>
+    </div>
+    )
+}
+
+export default IndexPage
