930861b36ae3599e5cfc814bb8d718e5f559c403
André Gonçalves <andgcv@gmail.com>
2020-01-12 10:05:11 +0000

    Had to update all due to some issues

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-const Button = styled.button`
+/* TODO: 3 Buttons, 1 small, 1 medium, 1 large
+   Each of these buttons will have media queries */
+const StyledButton = styled.button`
     background-color: ${props => props.theme.colors.vibrantBlue};
     color: ${props => props.theme.colors.lightSkyBlue};
     min-width: 25vh;
@@ -12,10 +16,25 @@ const Button = styled.button`
     padding: 0.25em 1em;
     border: none;
     box-shadow: 5px 5px ${props => props.theme.colors.lightSkyBlue};
+
+    & img {
+        height: 2vh;
+    }
 `
 
-export default (props) => (
-    <Button>
-        {props.text}
-    </Button>
-)
+// TODO: Will take in text (required) and an optional icon
+const Button = (props) => {
+    return (
+        <StyledButton >
+            <FontAwesomeIcon icon={faCoffee} />
+            {props.text}
+        </StyledButton>
+    )
+}
+
+/*Button.propTypes = {
+    text: React.PropTypes.string.isRequired,
+    icon: React.PropTypes.string
+}*/
+
+export default Button
