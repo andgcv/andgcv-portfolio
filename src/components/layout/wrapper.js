6a5bd685c4fd88e02a57a6ff6bbfd35b3d326694
André Gonçalves <andgcv@gmail.com>
2020-01-28 12:08:24 +0000

    Finished styling the 404 page (for now), extended Wrapper layout component

--- src/components/layout/wrapper.js
+++ src/components/layout/wrapper.js
@@ -17,17 +17,28 @@ const ContactWrapper = styled(SectionWrapper)`
     min-height: 90vh !important;
 `
 
+const NotFoundWrapper = styled(SectionWrapper)`
+    justify-content: center;
+    align-items: center;
+`
+
 const Wrapper = (props) => {
-    // If the wrapper is being used in the Home section, serve the <HomeWrapper> instead
+    // If the wrapper is being used in the Home section, serve the <HomeWrapper>
     if (props.home) {
         return (
             <HomeWrapper id="home-section">{props.children}</HomeWrapper>
         )
-    // Else, serve the generic <SectionWrapper>
+    // If the wrapper is being used in the Contact section, serve the <ContactWrapper>
     } else if (props.contact) {
         return (
             <ContactWrapper id="contact-section">{props.children}</ContactWrapper>
         )
+    // If the wrapper is being used in the 404 Page, serve the <NotFoundWrapper>
+    } else if (props.notFound) {
+        return(
+            <NotFoundWrapper>{props.children}</NotFoundWrapper>
+        )
+    // Else, serve the generic <SectionWrapper>
     } else {
         return (
             <SectionWrapper id={props.id}>{props.children}</SectionWrapper>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,62 @@
 import React from 'react'
+import styled from 'styled-components'
+// Styles
+import Theme from '../components/ui/theme'
+import GlobalStyles from '../components/ui/global-styles'
+// Layout
+import Wrapper from '../components/layout/wrapper'
+
+const NotFoundContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & p {
+        font-size: 1.2rem;
+        align-text: center;
+        color: ${props => props.theme.colors.desaturatedSkyBlue};
+    }
+
+    & a {
+        font-size: 1.2rem;
+        font-weight: 600;
+        color: ${props => props.theme.colors.lightSkyBlue};
+        text-decoration: underline;
+
+        & :hover {
+            color: ${props => props.theme.colors.vibrantBlue};
+        }
+
+        & :active {
+            color: ${props => props.theme.colors.vibrantBlue};
+        }
+    }
+`
+
+const StatusCode = styled.h3`
+    font-size: 11rem;
+    color: ${props => props.theme.colors.vibrantBlue};
+    margin: 0;
+
+    & span {
+        color: ${props => props.theme.colors.lightSkyBlue};
+        margin: 0 6vw;
+    }
+`
 
 const NotFoundPage = () => {
     return (
-        <h1>404 - Page not found.</h1>
+        <Theme>
+            <GlobalStyles />
+            <Wrapper notFound>
+                <NotFoundContainer>
+                    <StatusCode><span>&#x7b;</span>&#x22;404&#x22;<span>&#x7d;</span></StatusCode>
+                    <p>That was probably a mistake on my part. Here, let me help you out:</p>
+                    <a href="/">Take me back</a>
+                </NotFoundContainer>
+            </Wrapper>
+        </Theme>
     )
 }
 
