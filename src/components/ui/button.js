9c25bbd8a7cbacf3e7764fd2a7800ac732251f34
André Gonçalves <andgcv@gmail.com>
2020-02-02 16:15:47 +0000

    Added animations to Buttons while hovering

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { useSpring, animated } from 'react-spring'
 // Icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // Navigation
@@ -9,7 +10,7 @@ import { Link } from 'react-scroll'
 
 /* TODO: 3 Buttons, 1 small, 1 medium, 1 large
    Each of these buttons will have media queries */
-const StyledButton = styled.button`
+const StyledButton = styled(animated.button)`
     background-color: ${props => props.theme.colors.vibrantBlue};
     color: ${props => props.theme.colors.lightSkyBlue};
     width: 15vw;
@@ -95,11 +96,22 @@ const StyledButton = styled.button`
 `
 
 const Button = (props) => {
+    const [hover, setHover] = useState(false);
+    const ButtonSpring = useSpring({
+        config: {
+            tension: 350,
+            friction: 8
+        },
+        to: {
+            transform: `scale(${hover ? 1.1 : 1})`
+        }
+    })
+
     // If the Button's goal is a scroll to another section, then return a scroll link
     if (props.scroll) {
         return (
             <Link to={props.destination} smooth={true} spy={true}>
-                <StyledButton isSmall={props.small} isBig={props.big}>
+                <StyledButton style={ButtonSpring} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} isSmall={props.small} isBig={props.big}>
                     <FontAwesomeIcon icon={props.icon} style={{marginRight: 1 + 'vw'}} />
                     {props.text}
                 </StyledButton>
@@ -109,7 +121,7 @@ const Button = (props) => {
     } else if (props.email) {
         return (
             <a href={props.destination} target="_blank" rel="noopener noreferrer">
-                <StyledButton isSmall={props.small} isBig={props.big}>
+                <StyledButton style={ButtonSpring} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} isSmall={props.small} isBig={props.big}>
                     <FontAwesomeIcon icon={props.icon} style={{marginRight: 1 + 'vw'}} />
                     {props.text}
                 </StyledButton>
@@ -119,7 +131,7 @@ const Button = (props) => {
     } else {
         return (
             <a href={props.destination} target="_blank" rel="noopener noreferrer">
-                <StyledButton isSmall={props.small} isBig={props.big}>
+                <StyledButton style={ButtonSpring} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} isSmall={props.small} isBig={props.big}>
                     <FontAwesomeIcon icon={props.icon} style={{marginRight: 1 + 'vw'}} />
                     {props.text}
                 </StyledButton>
